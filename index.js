dc330371f4a5bd52be3063b9a3422e4f857c2fa7
rayssassousa20 <rayssbtx1@gmail.com>
2024-01-15 09:09:02 -0300

    versão final do desafio de felipão da DIO

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-let name = "rayssa"
-console.log(nome)
+let nomeJogador = "Rayssa"
+let quantXP = 15000
+let classificadorHeroi
+
+if (quantXP <= 1000){
+    classificadorHeroi = "Ferro"
+} else if ((quantXP >= 1001) && (quantXP <= 2000)){
+    classificadorHeroi = "Bronze"
+} else if ((quantXP >= 2001)&& (quantXP <= 5000)){
+    classificadorHeroi = "Prata"
+} else if ((quantXP >=5001) && (quantXP <= 7000)){
+    classificadorHeroi ="Ouro"
+} else if ((quantXP >= 7001)&& (quantXP <=8000)){
+    classificadorHeroi ="Platina"
+} else if ((quantXP >=8001) && (quantXP <= 9000)){
+    classificadorHeroi = "Ascendente"
+} else if ((quantXP >=9001)&& (quantXP <= 10000)){
+    classificadorHeroi = "Imortal"
+}else {
+    classificadorHeroi = "Radiante"
+}
+
+console.log("O Herói de nome: " + nomeJogador + " está no nível de: " + classificadorHeroi)
